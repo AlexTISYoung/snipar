7d36b6d83665c358c23809b1b778f502f8e6e37f
ay <alextisyoung@gmail.com>
2018-12-21 14:44:03 +0000

    pGWAS first attempt

--- bin/pGWAS.py
+++ bin/pGWAS.py
@@ -254,7 +254,7 @@ if __name__ == '__main__':
 
     ############### Loop through loci and fit models ######################
     print('Fitting models for genome-wide SNPs')
-    for loc in xrange(0,10):
+    for loc in xrange(0,100):
         print(str(loc))
         alpha_out = 'NA\tNA\tNA\tNA\tNA\tNA\t\tNA\tNA\tNA\n'
         # Filler for output if locus doesn't pass thresholds
