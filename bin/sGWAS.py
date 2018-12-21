627a7cb8507b1301a4e33b39439538738ac3d554
ay <alextisyoung@gmail.com>
2018-12-21 08:41:54 +0000

    sGWAS first attempt

--- bin/sGWAS.py
+++ bin/sGWAS.py
@@ -259,6 +259,7 @@ if __name__ == '__main__':
                 X_l[:,2] = g_mean
                 model_l = sibreg.model(y_l,X_l,fam_l)
                 optim_l = model_l.optimize_model(np.array([null_optim['sigma2'],null_optim['tau']]))
+                code.interact(local=locals())
                 if optim_l['success']:
                     alpha_l = model_l.alpha_mle(optim_l['tau'],optim_l['sigma2'],compute_cov = True)
                     alpha_out = str(n_loc)+'\t'+vector_out(alpha_l)
