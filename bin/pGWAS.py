9536fb2b4d552d6bc84a5024e374c8b94df18eb1
ay <alextisyoung@gmail.com>
2019-05-28 16:16:53 +0100

    rework for new imputed format

--- bin/pGWAS.py
+++ bin/pGWAS.py
@@ -10,10 +10,10 @@ import h5py, argparse, code
 def neglog10pval(x,df):
     return -np.log10(np.e)*chi2.logsf(x,df)
 
-def vector_out(alpha_mle, no_sib, n_X, digits=6):
-##Output parameter estimates along with standard errors ##
+def vector_out(n,alpha_l, no_sib, n_X, digits=6):
+    ##Output parameter estimates along with standard errors ##
     ## Calculate test statistics
-    if args.no_sib:
+    if no_sib:
         X_length = n_X+2
     else:
         X_length = n_X+3
@@ -21,9 +21,9 @@ def vector_out(alpha_mle, no_sib, n_X, digits=6):
     alpha_cov = alpha_l[1][n_X:X_length,n_X:X_length]
     alpha_ses = np.sqrt(np.diag(alpha_cov))
     alpha_corr = np.dot(np.diag(1/alpha_ses).dot(alpha_cov),np.diag(1/alpha_ses))
-    alpha_out = str(round(alpha_est[0],digits))+'\t'+str(round(alpha_ses[0],digits))+'\t'
+    alpha_out = str(n)+'\t'+str(round(alpha_est[0],digits))+'\t'+str(round(alpha_ses[0],digits))+'\t'
     alpha_out += str(round(alpha_est[1],digits))+'\t'+str(round(alpha_ses[1],digits))+'\t'
-    if args.no_sib:
+    if no_sib:
         alpha_out += str(round(alpha_corr[0, 1], digits)) + '\n'
     else:
         alpha_out += str(round(alpha_est[2],digits))+'\t'+str(round(alpha_ses[2],digits))+'\t'
@@ -87,7 +87,6 @@ if __name__ == '__main__':
     parser.add_argument('--phen_index',type=int,help='If the phenotype file contains multiple phenotypes, which phenotype should be analysed (default 1, first)',
                         default=1)
     parser.add_argument('--min_maf',type=float,help='Ignore SNPs with minor allele frequency below min_maf (default 0.05)',default=0.05)
-    parser.add_argument('--max_maf',type=float,help='Ignore SNPs with minor allele frequency above max_maf (default 0.5)',default=0.5)
     parser.add_argument('--missing_char',type=str,help='Missing value string in phenotype file (default NA)',default='NA')
     parser.add_argument('--max_missing',type=float,help='Ignore SNPs with greater percent missing calls than max_missing (default 5)',default=5)
     parser.add_argument('--append',action='store_true',default=False,help='Append results to existing output file with given outprefix (default overwrites existing')
@@ -100,7 +99,7 @@ if __name__ == '__main__':
     ####################### Read in data #########################
     #### Read phenotype ###
     pheno = Pheno(args.phenofile, missing=args.missing_char).read()
-    #pheno = Pheno('gwas/h2_0.4/h2_0.4.ped', missing='NA').read()
+    #pheno = Pheno('phenotypes/eduyears_resid.ped', missing='NA').read()
     y = np.array(pheno.val)
     pheno_ids = np.array(pheno.iid)
     if y.ndim == 1:
@@ -140,6 +139,7 @@ if __name__ == '__main__':
 ### Read pedigree file ###
     ### Load pedigree
     ped = np.loadtxt(args.sibped, dtype='S20', skiprows=1)
+    #ped = np.loadtxt('23andme/sibs.ped', dtype='S20', skiprows=1)
     sibs = np.unique(ped[:, 1])
 
     ### Create family dictionary
@@ -155,6 +155,7 @@ if __name__ == '__main__':
 ### Read sibling genotypes ###
     #### Load genotypes
     gts_f = Bed(args.sibgts)
+    #gts_f = Bed('genotypes/chr_22.bed')
     gts_ids = gts_f.iid
     # Build dict
     id_dict = {}
@@ -206,6 +207,7 @@ if __name__ == '__main__':
     ### Read imputed parental genotypes ###
     print('Reading imputed parental genotype file')
     pargts_f = h5py.File(args.pargts,'r')
+    #pargts_f = h5py.File('23andme/chr_22.hdf5','r')
     # get families
     par_fams = np.array(pargts_f['families'])
     # build family dictionary
@@ -230,7 +232,7 @@ if __name__ == '__main__':
         pargts = pargts[:,in_sib_sid]
         par_sid = par_sid[in_sib_sid]
         gts = gts[:,sib_sid_indices]
-        print(str(gts.shape[0])+' variants in common between parental and sibling genotypes')
+        print(str(gts.shape[1])+' variants in common between parental and sibling genotypes')
     else:
         raise(ValueError('No variants in common between sibling and parental genotypes'))
 
@@ -257,33 +259,48 @@ if __name__ == '__main__':
     nfam = len(fams_with_data)
     print('Sample of '+str(sample_size)+' comprised of '+str(nfam)+' families with at least two siblings genotyped and at least one sibling phenotyped')
 
+    print('Forming family-wise genotype matrix')
     if args.no_sib:
-        G = np.zeros((sample_size, 2, gts.shape[1]))
-        p_index = 1
+        gsize = 1
     else:
-        G = np.zeros((sample_size, 3, gts.shape[1]))
-        p_index = 2
+        gsize = 2
+    G = ma.array(np.zeros((sample_size, gsize, gts.shape[1]),dtype=np.int8),
+                 mask=np.zeros((sample_size, gsize, gts.shape[1]), dtype=bool))
+    G_par = np.zeros((sample_size, gts.shape[1]), dtype=np.float32)
     start = 0
     y_new = np.zeros((sample_size))
-    X_new = np.zeros((sample_size))
+    X_new = np.zeros((sample_size,X.shape[1]))
     for i in xrange(0,nfam):
         fam = fams_with_data[i]
         end = start + len(sibs_with_pheno[i])
         # Fill in phenotype vector
-        y_new[start:end] = y[np.array([pheno_id_dict[x] for x in sibs_with_pheno[i]])]
+        pheno_indices_i = np.array([pheno_id_dict[x] for x in sibs_with_pheno[i]])
+        y_new[start:end] = y[pheno_indices_i]
+        # Fill in phenotype vector
+        X_new[start:end,:] = X[pheno_indices_i,:]
         # Fill in proband genotype column
-        G[start:end,0,:] = gts[np.array([id_dict[x] for x in sibs_with_pheno[i]])]
+        gindices = np.array([id_dict[x] for x in sibs_with_pheno[i]])
+        G[start:end,0,:] = gts[gindices,:]
+        G.mask[start:end,0,:] = gts.mask[gindices,:]
         # Fill in parental genotype column
+        par_index_i = par_fam_dict[fams_with_data[0]]
         for j in xrange(start,end):
-            G[j,p_index,:] = pargts[par_fam_dict[fams_with_data[0]],:]
+            G_par[j,:] = pargts[par_index_i,:]
         if not args.no_sib:
             # Fill in average sibling genotype column
-            gsum = np.sum(gts[np.array([id_dict[x] for x in sibs_with_geno[i]]),:],axis=0)
+            gindices = np.array([id_dict[x] for x in sibs_with_geno[i]])
+            gsum = ma.sum(gts[gindices,:],axis=0)
+            gmask = np.sum(gts[gindices, :].mask, axis=0) > 0
             for s in range(0,sibs_with_pheno[i].shape[0]):
                 sname = sibs_with_pheno[i][s]
-                G[s,1,:] = gsum-gts[id_dict[sname],:]
-            G[:,1,:] = G[:,1,:]/float(sibs_with_geno[i].shape[0]-1)
+                G[start+s,1,:] = gsum-gts[id_dict[sname],:]
+                G[start+s,1,:].mask = gmask
+            G[start:end,1,:] = G[start:end,1,:]/float(sibs_with_geno[i].shape[0]-1)
+        start = end
 
+    y = y_new
+    X = X_new
+    families = np.array(fams_with_data)
 
 ######### Initialise output files #######
     ## Output file
@@ -292,6 +309,7 @@ if __name__ == '__main__':
     else:
         write_mode='wb'
     outfile=open(args.outprefix+'.models.gz',write_mode)
+    #outfile = open('23andme/test.models.gz', 'wb')
     if not args.append:
         if args.no_sib:
             header = 'SNP\tfrequency\tn\tdelta\tdelta_SE\tbeta\tbeta_SE\t'
@@ -307,7 +325,7 @@ if __name__ == '__main__':
     # Optimize null model
     sigma_2_init = np.var(y)*args.tau_init/(1+args.tau_init)
     #sigma_2_init = np.var(y) * 1 / (1 + 1)
-    null_model = sibreg.model(y, X, pheno_ids[:,0])
+    null_model = sibreg.model(y, X, families)
     null_optim = null_model.optimize_model(np.array([sigma_2_init,args.tau_init]))
     print('Within family variance estimate: '+str(round(null_optim['sigma2']/null_optim['tau'],4)))
     print('Residual variance estimate: ' + str(round(null_optim['sigma2'],4)))
@@ -334,33 +352,41 @@ if __name__ == '__main__':
         # Residual y
         y=y-X.dot(null_alpha[0])
         # Reformulate fixed_effects
-        X=np.ones((int(n),1))
+        X=np.ones((sample_size,1))
         n_X=1
 
     ############### Loop through loci and fit models ######################
     print('Fitting models for genome-wide SNPs')
+    # Optimize model for SNP
+    if args.no_sib:
+        X_length = n_X + 2
+    else:
+        X_length = n_X + 3
+    freqs = ma.mean(G[:,0,:],axis=0)/2.0
+    missingness = ma.mean(G.mask[:,0,:],axis=0)
     for loc in xrange(0,G.shape[2]):
         if args.no_sib:
             alpha_out = 'NA\tNA\tNA\tNA\tNA\n'
         else:
             alpha_out = 'NA\tNA\tNA\tNA\tNA\tNA\tNA\tNA\tNA\n'
-        # Optimize model for SNP
-        if args.no_sib:
-            X_length = n_X+2
-        else:
-            X_length = n_X+3
-        X_l = np.ones((y.shape[0], X_length))
-        X_l[:, n_X:X_length] = G[:, :, loc]
-        model_l = sibreg.model(y,X_l,pheno_ids[:,0])
-        if not args.fix_VC:
-            optim_l = model_l.optimize_model(np.array([null_optim['sigma2'], null_optim['tau']]))
-            if optim_l['success']:
-                alpha_l = model_l.alpha_mle(optim_l['tau'], optim_l['sigma2'], compute_cov=True)
+        if freqs[loc] > args.min_maf and freqs[loc] < (1-args.min_maf) and missingness[loc] < args.max_missing:
+            # Find NAs
+            if args.no_sib:
+                not_nans = np.logical_not(G[:,0,loc].mask)
             else:
-                print('Maximisation of likelihood failed for for ' + sid[loc])
-        else:
-            alpha_l = model_l.alpha_mle(null_optim['tau'], null_optim['sigma2'], compute_cov=True)
-        alpha_out = vector_out(alpha_l, args.no_sib, n_X)
-        print(sid[loc]+' finished successfully')
-        outfile.write(sid[loc] +'\t'+str(freqs[loc])+'\t'+str(int(n)) +'\t'+alpha_out+'\n')
+                not_nans = np.logical_not(G[:,1,loc].mask)
+            n_l = np.sum(not_nans)
+            X_l = np.ones((n_l, X_length),dtype=np.float32)
+            X_l[:, n_X:X_length] = G[not_nans, :, loc]
+            model_l = sibreg.model(y[not_nans],X_l,pheno_ids[:,0])
+            if not args.fix_VC:
+                optim_l = model_l.optimize_model(np.array([null_optim['sigma2'], null_optim['tau']]))
+                if optim_l['success']:
+                    alpha_l = model_l.alpha_mle(optim_l['tau'], optim_l['sigma2'], compute_cov=True)
+                else:
+                    print('Maximisation of likelihood failed for for ' + sid[loc])
+            else:
+                alpha_l = model_l.alpha_mle(null_optim['tau'], null_optim['sigma2'], compute_cov=True)
+            alpha_out = vector_out(n_l,alpha_l, args.no_sib, n_X)
+        outfile.write(sid[loc] +'\t'+str(freqs[loc])+'\t'+alpha_out+'\n')
     outfile.close()
