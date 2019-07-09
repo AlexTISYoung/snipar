c6083388aabd5297d2e19333fe46fbc3a13e377e
ay <alextisyoung@gmail.com>
2019-07-09 16:49:32 +0100

    trait simulation

--- bin/simulate_trait.py
+++ bin/simulate_trait.py
@@ -60,4 +60,4 @@ y = a*a_factor+np.sqrt(args.c2)*(fam_effects/np.std(fam_effects,axis=0))+np.sqrt
 
 # Write phenotype
 yout = np.hstack((gts_ids,np.array(y,dtype=str)))
-np.savetxt(args.outprex+'.ped',yout,fmt='%s')
+np.savetxt(args.outprefix+'.ped',yout,fmt='%s')
