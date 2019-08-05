c78ebdf54b8d7fc4c679fae52a88ca32bc398b75
ay <alextisyoung@gmail.com>
2019-08-05 16:16:04 +0100

    tutorial

--- bin/impute_from_sibs_hdf5.py
+++ bin/impute_from_sibs_hdf5.py
@@ -107,6 +107,7 @@ for i in xrange(0,ped.shape[0]):
 ibd_f = h5py.File(args.ibd,'r')
 ibd = np.array(ibd_f['ibd'])
 ibd_fams = np.array(ibd_f['ibd_fams'],dtype='S20')
+ibd_fams=np.array(ibd_fams,dtype='str')
 ibd_fam_dict = {}
 for i in range(0,ibd_fams.shape[0]):
     ibd_fam_dict[ibd_fams[i]] = i
