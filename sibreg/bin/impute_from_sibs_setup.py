a853b29a44ed9ca6a47e1a6684c7d9bf246e2b08
Moeen Nehzati <nehzati@usc.edu>
2020-03-17 11:08:43 -0700

    Add numpy path to impute_from_sibs_setup

--- sibreg/bin/impute_from_sibs_setup.py
+++ sibreg/bin/impute_from_sibs_setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-
+import numpy
 setup(name='sib imputation',
-      ext_modules=cythonize("sibreg/bin/impute_from_sibs.pyx", annotate = True, language='c++'))
+      ext_modules=cythonize("sibreg/bin/impute_from_sibs.pyx", annotate = True, language='c++', include_path=[numpy.get_include()]),
+      include_dirs=[numpy.get_include()]
+      )
