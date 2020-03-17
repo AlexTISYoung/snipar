f9b1e432e46f25defecd40168fe75d56554710dd
Moeen Nehzati <nehzati@usc.edu>
2020-03-17 13:00:32 -0700

    Make tests discoverable from unittest

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,8 @@
-from tests.test_impute_from_sibs import *
+import os
+os.system("python tests/impute_from_sibs_setup.py build_ext --inplace")
+
+from test_generated import *
+from test_sibreg import *
+from test_impute_from_sibs import *
+if __name__ == '__main__':
+    unittest.main()
