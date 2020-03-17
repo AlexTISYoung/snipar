e7fcc8ffc09d979a6eda74bd26e1731451569374
Moeen Nehzati <nehzati@usc.edu>
2020-03-17 12:57:36 -0700

    Debug the problem of building cpp

--- sibreg/bin/impute_from_sibs.pyx
+++ sibreg/bin/impute_from_sibs.pyx
@@ -1,3 +1,4 @@
+# distutils: language = c++
 #!/well/kong/users/wiw765/anaconda2/bin/python
 import numpy as np
 import pandas as pd
--- sibreg/bin/impute_from_sibs_setup.py
+++ sibreg/bin/impute_from_sibs_setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 setup(name='sib imputation',
-      ext_modules=cythonize("sibreg/bin/impute_from_sibs.pyx", annotate = True, language='c++', include_path=[numpy.get_include()]),
+      ext_modules=cythonize("sibreg/bin/impute_from_sibs.pyx", annotate = True, language='c++'),
       include_dirs=[numpy.get_include()]
       )
