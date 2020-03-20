72d72499b8bb33837ea680fec8363b98bc3612ba
moeen-n <nehzati@usc.edu>
2020-03-20 15:20:59 -0400

    Add test_commandline to tests pipeline

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,5 +4,7 @@ os.system("python tests/impute_from_sibs_setup.py build_ext --inplace")
 from test_generated import *
 from test_sibreg import *
 from test_impute_from_sibs import *
+from test_commandline import *
+
 if __name__ == '__main__':
     unittest.main()
