6e11e0088ebe4711efdfe9eb4a81528474cfa0cb
moeen-n <nehzati@usc.edu>
2020-03-20 14:57:39 -0400

    Add end to end tests with tests/test_commandline.py

---
+++ tests/test_commandline.py
@@ -0,0 +1,75 @@
+import unittest
+import subprocess
+from test_imputation import imputation_test
+
+class TestCommanline(unittest.TestCase):
+    p_value_threshold = 0.01
+    def test_impute_runner_with_pedigree(self):
+        #TODO test for multiple chromosomes
+        command = ["python",
+                   "sibreg/bin/impute_runner.py",
+                   "1",
+                   "2",
+                   "test_data/sample.segments.gz",
+                   "test_data/sample",
+                   "--pedigree", "test_data/sample.ped",
+                   "--out_prefix", "test_sample_imputed",
+                   ]
+        subprocess.check_call(command)
+    
+    def test_impute_runner_with_pedigree_control(self):
+        #TODO test for multiple chromosomes
+        command = ["python",
+                   "sibreg/bin/impute_runner.py",
+                   "-c",
+                   "1",
+                   "2",
+                   "test_data/sample.segments.gz",
+                   "test_data/sample",
+                   "--pedigree", "test_data/sample.ped",
+                   "--out_prefix", "test_sample_imputed",
+                   ]
+        subprocess.check_call(command)
+        coef, z, p_value = imputation_test([1],
+                imputed_prefix = "test_sample_imputed",
+                expected_prefix = "test_data/sample",
+                pedigree_address = "test_data/__controlledsample.ped"
+                )
+        self.assertGreaterEqual(p_value, self.p_value_threshold)
+
+    def test_impute_runner_with_king(self):
+        #TODO test for multiple chromosomes
+        command = ["python",
+                   "sibreg/bin/impute_runner.py",
+                   "1",
+                   "2",
+                   "test_data/sample.segments.gz",
+                   "test_data/sample",
+                   "--king", "test_data/sample.king",
+                   "--agesex", "test_data/sample.agesex",
+                   "--out_prefix", "test_sample_imputed",
+                   ]
+        subprocess.check_call(command)
+
+    # TODO handle nothing to impute errors
+    def test_impute_runner_with_king_control(self):
+        #TODO test for multiple chromosomes
+        command = ["python",
+                   "sibreg/bin/impute_runner.py",
+                   "-c",
+                   "1",
+                   "2",
+                   "test_data/sample.segments.gz",
+                   "test_data/sample",
+                   "--king", "test_data/sample.king",
+                   "--agesex", "test_data/sample.agesex",
+                   "--out_prefix", "test_sample_imputed",
+                   ]
+        subprocess.check_call(command)
+        coef, z, p_value = imputation_test([1],
+                imputed_prefix = "test_sample_imputed",
+                expected_prefix = "test_data/sample",
+                pedigree_address = "test_data/__controlled__tmp_pedigree"
+                )
+        self.assertGreaterEqual(p_value, self.p_value_threshold)
+
