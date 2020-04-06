af51f1ac44821ce47a2d44b29052d53011289c30
moeen-n <nehzati@usc.edu>
2020-04-06 14:47:48 -0400

    This should have been on the last comment

--- tests/generate_test_population.sh
+++ tests/generate_test_population.sh
@@ -1,33 +1,38 @@
 python example/simulate_pop.py 1000 0.05 450 350 50 0 "outputs/tmp/t__t"
 python -c 'import pandas as pd;
-ped = pd.read_csv("outputs/tmp/t__t_fams.ped", names = ["FID", "IID", "FATHER_ID", "MOTHER_ID"], sep = " ");
+ped = pd.read_csv("outputs/tmp/t__t_fams.ped", sep = " ");
 remove = pd.read_csv("outputs/tmp/t__t_remove.txt", sep = " ", names = ["counter", "id"]);
 ped = ped[~ped["IID"].isin(remove["id"])];
-ped.to_csv("test_data/sample.ped", index = False, sep = " ")
-sibships = ped.groupby(["FID","FATHER_ID", "MOTHER_ID"]).agg({"IID":list}).reset_index()
-ids = set(ped["IID"].tolist())
-kinship = []
+ped.to_csv("test_data/sample.ped", index = False, sep = " ");
+sibships = ped.groupby(["FID","FATHER_ID", "MOTHER_ID"]).agg({"IID":list}).reset_index();
+ids = set(ped["IID"].tolist());
+kinship = [];
 for index, row in sibships.iterrows():
     for i in range(1,len(row["IID"])):
         for j in range(i):
-            kinship.append([row["FID"], row["IID"][i], row["FID"], row["IID"][j], "FS"])
+            kinship.append([row["FID"], row["IID"][i], row["FID"], row["IID"][j], "FS"]);
 for index, row in ped.iterrows():
     if row["FATHER_ID"] in ids:
-        kinship.append([row["FID"], row["IID"], row["FID"], row["FATHER_ID"],"PO"])
+        kinship.append([row["FID"], row["IID"], row["FID"], row["FATHER_ID"],"PO"]);
     if row["MOTHER_ID"] in ids:
-        kinship.append([row["FID"], row["IID"], row["FID"], row["MOTHER_ID"],"PO"])
-kinship_df = pd.DataFrame(kinship, columns = ["FID1", "ID1", "FID2", "ID2", "InfType"])
-kinship_df.to_csv("test_data/sample.king", sep = "\t", index = False)
-agesex = ped.copy()
-agesex["is_father"] = agesex["IID"].isin(agesex["FATHER_ID"])
-agesex["is_mother"] = agesex["IID"].isin(agesex["MOTHER_ID"])
-agesex["sex"] = "F"
-agesex.loc[agesex["is_father"],"sex"] = "M"
-agesex["age"] = 10
-agesex.loc[agesex["is_father"]|agesex["is_mother"],"age"] = 100
-agesex.to_csv("test_data/sample.agesex", sep = " ", index = False)
+        kinship.append([row["FID"], row["IID"], row["FID"], row["MOTHER_ID"],"PO"]);
+kinship_df = pd.DataFrame(kinship, columns = ["FID1", "ID1", "FID2", "ID2", "InfType"]);
+kinship_df.to_csv("test_data/sample.king", sep = "\t", index = False);
+agesex = ped.copy();
+agesex["is_father"] = agesex["IID"].isin(agesex["FATHER_ID"]);
+agesex["is_mother"] = agesex["IID"].isin(agesex["MOTHER_ID"]);
+agesex["sex"] = "F";
+agesex.loc[agesex["is_father"],"sex"] = "M";
+agesex["age"] = 10;
+agesex.loc[agesex["is_father"]|agesex["is_mother"],"age"] = 100;
+agesex.to_csv("test_data/sample.agesex", sep = " ", index = False);
+king_chr1 = pd.read_csv("outputs/tmp/t__t.segments.gz", sep = "\t");
+king_chr2 = king_chr1.copy();
+king_chr2["Chr"] = 2;
+king_chr12 = pd.concat([king_chr1, king_chr2]);
+king_chr12.to_csv("test_data/sample.segments.gz", sep = "\t", index = False);
+print(king_chr12);
 '
-mv outputs/tmp/t__t.segments.gz test_data/sample.segments.gz
 plink/plink --file outputs/tmp/t__t --make-bed --out outputs/tmp/t__t
 plink/plink --bfile outputs/tmp/t__t --remove outputs/tmp/t__t_remove.txt --make-bed --out test_data/sample1
 cp test_data/sample1.bed test_data/sample2.bed 
