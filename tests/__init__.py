081b3714f1714dd04cf133a77ee24ccedd65d582
moeen-n <nehzati@usc.edu>
2020-04-09 13:57:48 -0400

    Correct imports in tests init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,12 @@
 import os
 os.system("python tests/impute_from_sibs_setup.py build_ext --inplace")
 os.system("python sibreg/bin/impute_from_sibs_setup.py build_ext --inplace")
-
-from test_generated import *
-from test_sibreg import *
-from test_impute_from_sibs import *
-from test_commandline import *
-from test_pedigree_creation import *
+print("HEY")
+from .test_generated import *
+from .test_sibreg import *
+from .test_impute_from_sibs import *
+from .test_commandline import *
+from .test_pedigree_creation import *
 
 if __name__ == '__main__':
     unittest.main()
