901497d56afe8b7f1e89bd2c30a1df71c94af8e2
Moeen Nehzati <nehzati@usc.edu>
2020-04-22 15:25:56 -0700

    Change copy command in tests init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 import os
 os.system("python tests/impute_from_sibs_setup.py build_ext --inplace")
 os.system("python sibreg/bin/impute_from_sibs_setup.py build_ext --inplace")
-os.system("cp impute_from_sibs.cpython-37m-x86_64-linux-gnu.so sibreg/bin/")
+os.system("cp impute_from_sibs*.so sibreg/bin/")
 from tests.test_generated import *
 from tests.test_sibreg import *
 # from tests.test_impute_from_sibs import *
