145f1dd34bdd23dcd752eca762c2d9634b446703
moeen-n <nehzati@usc.edu>
2020-04-22 14:52:38 -0400

    Add commandline tests for multithreading

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -5,7 +5,6 @@ from tests.test_imputation import imputation_test
 class TestCommanline(unittest.TestCase):
     p_value_threshold = 0.01
     def test_impute_runner_with_pedigree(self):
-        #TODO test for multiple chromosomes
         command = ["python",
                    "impute_runner.py",
                    "1",
@@ -18,7 +17,6 @@ class TestCommanline(unittest.TestCase):
         subprocess.check_call(command)
     
     def test_impute_runner_with_pedigree_control(self):
-        #TODO test for multiple chromosomes
         command = ["python",
                    "impute_runner.py",
                    "-c",
@@ -38,7 +36,6 @@ class TestCommanline(unittest.TestCase):
         self.assertGreaterEqual(p_value[1], self.p_value_threshold)
 
     def test_impute_runner_with_king(self):
-        #TODO test for multiple chromosomes
         command = ["python",
                    "impute_runner.py",
                    "1",
@@ -53,7 +50,6 @@ class TestCommanline(unittest.TestCase):
 
     # TODO handle nothing to impute errors
     def test_impute_runner_with_king_control(self):
-        #TODO test for multiple chromosomes
         command = ["python",
                    "impute_runner.py",
                    "-c",
@@ -64,6 +60,27 @@ class TestCommanline(unittest.TestCase):
                    "--king", "test_data/sample.king",
                    "--agesex", "test_data/sample.agesex",
                    "--out_prefix", "outputs/tmp/test_sample_imputed",
+                   ]
+        subprocess.check_call(command)
+        coef, z, p_value = imputation_test([1, 2],
+                imputed_prefix = "outputs/tmp/test_sample_imputed",
+                expected_prefix = "test_data/sample",
+                )
+        self.assertGreaterEqual(p_value[0], self.p_value_threshold)
+        self.assertGreaterEqual(p_value[1], self.p_value_threshold)
+
+
+    def test_impute_runner_with_pedigree_control_multicore(self):
+        command = ["python",
+                   "impute_runner.py",
+                   "-c",
+                   "1",
+                   "3",
+                   "test_data/sample.segments.gz",
+                   "test_data/sample",
+                   "--pedigree", "test_data/sample.ped",
+                   "--out_prefix", "outputs/tmp/test_sample_imputed",
+                   "--threads", "2",
                    ]
         subprocess.check_call(command)
         coef, z, p_value = imputation_test([1, 2],
