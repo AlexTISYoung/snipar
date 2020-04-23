50a7950c6f0891e0559aa2ac2774fd09f0447fdf
moeen-n <nehzati@usc.edu>
2020-04-23 13:38:46 -0400

    Change tests to be consistant with ~ bed file names and add tests for that

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -10,7 +10,7 @@ class TestCommanline(unittest.TestCase):
                    "1",
                    "3",
                    "test_data/sample.segments.gz",
-                   "test_data/sample",
+                   "test_data/sample~",
                    "--pedigree", "test_data/sample.ped",
                    "--out_prefix", "outputs/tmp/test_sample_imputed",
                    ]
@@ -23,7 +23,7 @@ class TestCommanline(unittest.TestCase):
                    "1",
                    "3",
                    "test_data/sample.segments.gz",
-                   "test_data/sample",
+                   "test_data/sample~",
                    "--pedigree", "test_data/sample.ped",
                    "--out_prefix", "outputs/tmp/test_sample_imputed",
                    ]
@@ -41,7 +41,7 @@ class TestCommanline(unittest.TestCase):
                    "1",
                    "3",
                    "test_data/sample.segments.gz",
-                   "test_data/sample",
+                   "test_data/sample~",
                    "--king", "test_data/sample.king",
                    "--agesex", "test_data/sample.agesex",
                    "--out_prefix", "outputs/tmp/test_sample_imputed",
@@ -56,7 +56,7 @@ class TestCommanline(unittest.TestCase):
                    "1",
                    "3",
                    "test_data/sample.segments.gz",
-                   "test_data/sample",
+                   "test_data/sample~",
                    "--king", "test_data/sample.king",
                    "--agesex", "test_data/sample.agesex",
                    "--out_prefix", "outputs/tmp/test_sample_imputed",
@@ -77,13 +77,33 @@ class TestCommanline(unittest.TestCase):
                    "1",
                    "3",
                    "test_data/sample.segments.gz",
-                   "test_data/sample",
+                   "test_data/sample~",
                    "--pedigree", "test_data/sample.ped",
                    "--out_prefix", "outputs/tmp/test_sample_imputed",
                    "--threads", "2",
                    ]
         subprocess.check_call(command)
         coef, z, p_value = imputation_test([1, 2],
+                imputed_prefix = "outputs/tmp/test_sample_imputed",
+                expected_prefix = "test_data/sample",
+                )
+        self.assertGreaterEqual(p_value[0], self.p_value_threshold)
+        self.assertGreaterEqual(p_value[1], self.p_value_threshold)
+
+    def test_impute_runner_with_pedigree_control_notilda(self):
+        command = ["python",
+                   "impute_runner.py",
+                   "-c",
+                   "1",
+                   "2",
+                   "test_data/sample.segments.gz",
+                   "test_data/sample1",
+                   "--pedigree", "test_data/sample.ped",
+                   "--out_prefix", "outputs/tmp/test_sample_imputed",
+                   "--threads", "2",
+                   ]
+        subprocess.check_call(command)
+        coef, z, p_value = imputation_test([1],
                 imputed_prefix = "outputs/tmp/test_sample_imputed",
                 expected_prefix = "test_data/sample",
                 )
