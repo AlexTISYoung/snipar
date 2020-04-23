6e43292626858698484f6b9e442d3fe003932bfb
moeen-n <nehzati@usc.edu>
2020-04-22 20:41:23 -0400

    Change setups to only use setuptools

--- sibreg/bin/impute_from_sibs_setup.py
+++ sibreg/bin/impute_from_sibs_setup.py
@@ -3,21 +3,13 @@
 Just run "python sibreg/bin/impute_from_sibs.py build_ext --inplace"
 """
 
-# from distutils.core import setup
-# from Cython.Build import cythonize
-# import numpy
-# setup(name='sib imputation',
-#       ext_modules=cythonize("sibreg/bin/impute_from_sibs.pyx", annotate = True, language='c++'),
-#       include_dirs=[numpy.get_include()]
-#       )
-
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 import numpy
 
 ext_modules = [
     Extension(
-        "impute_from_sibs",
+        "sibreg.bin.impute_from_sibs",
         ["sibreg/bin/impute_from_sibs.pyx"],
         extra_compile_args=['-fopenmp'],
         extra_link_args=['-fopenmp'],
