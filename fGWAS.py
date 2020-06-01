53decf22992cce2d70ece8bc0e02a5c6a093eb4e
ay <alextisyoung@gmail.com>
2020-06-01 14:43:22 -0700

    vectorized mle with cholesky based transform

--- fGWAS.py
+++ fGWAS.py
@@ -1,6 +1,4 @@
 #!/well/kong/users/wiw765/anaconda2/bin/python
-import numpy as np
-import numpy.ma as ma
 from pysnptools.snpreader import Bed, Pheno
 from sibreg import sibreg
 import h5py, argparse, os, time
@@ -128,6 +126,9 @@ if __name__ == '__main__':
     os.environ["OMP_NUM_THREADS"] = str(args.threads)  # export OMP_NUM_THREADS=4
     os.environ["OPENBLAS_NUM_THREADS"] = str(args.threads)   # export OPENBLAS_NUM_THREADS=4
     os.environ["MKL_NUM_THREADS"] = str(args.threads)   # export MKL_NUM_THREADS=6
+
+    import numpy as np
+    import numpy.ma as ma
 
     ######### Read Phenotype ########
     pheno = Pheno(args.phenofile, missing=args.missing_char).read()
