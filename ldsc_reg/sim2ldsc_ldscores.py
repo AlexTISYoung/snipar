f6a3220f6fb51437ff049abf1d363ece204eb544
unknown <hariharan.jayashankar@gmail.com>
2020-08-24 22:02:04 +0530

    making script to allow ldsc to read ld scores

--- ldsc_reg/sim2ldsc_ldscores.py
+++ ldsc_reg/sim2ldsc_ldscores.py
@@ -3,7 +3,7 @@ import ldsc_72 as ld2
 import numpy as np
 import pandas as pd
 import h5py
-from helperfuncs import *
+from scipy.stats import norm
 
 
 np.random.seed(123)
@@ -17,6 +17,27 @@ V = np.identity(2) * 0.8
 model = ld2.sibreg(S = S)
 model.simdata(V, N, simr = True)
 
+# === Outputing to LDSC Format - Direct Effects === #
+
+#snp = np.arange(1, len(theta_dir) + 1, 1)
+se = np.sqrt(S[:, 0, 0])/N
+zval = theta[:, 0]/se
+pval = 2*norm.sf(np.abs(zval))
+pval[np.where(pval < 1e-32)] = 1e-32
+
+
+simulated_data_out = pd.DataFrame({'chromosome' : np.array([22] * N),
+				   					'snp' : model.snp,
+                                    'pos' : model.pos,
+			           				'A1' : np.array(["A"] * N),
+			            			'A2' : np.array(["C"] * N),
+				     				'N' : N,
+                                    'b' : model.theta[:, 0],
+				     				'se' : model.S[:, 0, 0],
+				      				'p' : pval})
+
+
+simulated_data_out.to_csv("ldsc_reg/ldcsores/simdata2ldsc_dir.csv", sep = ' ')
 
 # main ldscore data
 ldscores = pd.DataFrame({'CHR' : np.array([22] * N),
