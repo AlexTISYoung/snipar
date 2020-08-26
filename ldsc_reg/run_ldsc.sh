074df3ee10f006df4a40b8cef0909d00a38ab097
Hariharan Jayashankar <harij@g01.nber.org>
2020-08-26 07:49:38 -0400

    fixed gen_output.py to get correct SE's and p-vals

--- ldsc_reg/run_ldsc.sh
+++ ldsc_reg/run_ldsc.sh
@@ -6,7 +6,7 @@ merge_alleles="/disk/genetics2/pub/data/PH3_Reference/w_hm3.snplist"
 python3 ldsc_reg/gen_output.py
 
 # Munging generated CSV's
-python3 ${ldsc_repo}/munge_sumstats.py --sumstats ldsc_reg/simulated_data_dir.csv --out ldsc_reg/simulated_data_dir  --ignore pos
+python3 ${ldsc_repo}/munge_sumstats.py --sumstats ldsc_reg/simulated_data_dir.csv --out ldsc_reg/simulated_data_dir
 #--merge-alleles ${merge_alleles}
 
 
