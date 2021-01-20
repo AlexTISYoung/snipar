fix error in computing inverse square root of covariance matrix

fGWAS.py
@@ -31,10 +31,10 @@ def match_phenotype(G,y,pheno_ids):
     return y
 
 def fit_null_model(y,fam_labels,tau_init = 1):
-    null_model = sibreg.model(y, np.ones((y.shape[0], 1)), fam_labels)
+    null_model = model(y, np.ones((y.shape[0], 1)), fam_labels)
     sigma_2_init = np.var(y) * tau_init / (1 + args.tau_init)
     null_optim = null_model.optimize_model(np.array([sigma_2_init, args.tau_init]))
-    return null_optim['sigma2'], null_optim['tau']
+    return null_model, null_optim['sigma2'], null_optim['tau']
 
 def transform_phenotype(L,y, fam_indices):
     # Mean normalise phenotype
@@ -112,6 +112,7 @@ if __name__ == '__main__':
     #### Filter SNPs ####
     print('Filtering based on MAF and missingness')
     G.filter_snps(args.min_maf, args.max_missing)
+    print(str(G.shape[2])+' SNPs that pass MAF and missingness filters')
     #### Fill NAs ####
     print('Imputing missing values with population frequencies')
     N_L = G.fill_NAs()
@@ -119,7 +120,7 @@ if __name__ == '__main__':
     y = match_phenotype(G,y,pheno_ids)
     #### Fit null model ####
     print('Estimating variance components')
-    sigma2, tau = fit_null_model(y,G.fams, tau_init = args.tau_init)
+    null_model, sigma2, tau = fit_null_model(y,G.fams, tau_init = args.tau_init)
     print('Family variance estimate: '+str(round(sigma2/tau,4)))
     print('Residual variance estimate: ' + str(round(sigma2,4)))
     ##### Transform genotypes and phenotypes ######

sibreg/sibreg.py
@@ -183,9 +183,9 @@ class model(object):
             Sigma_lab = sigma_u*np.ones((famsize,famsize))
             np.fill_diagonal(Sigma_lab,sigma_u+sigma2)
             vals, vectors = np.linalg.eigh(Sigma_lab)
-            vals = np.power(vals,-0.5)
-            sigma2_nsqrt[famsize] = vectors.dot(vals.dot(vectors.T))
-
+            vals = np.power(vals,0.25)
+            vectors = vectors/vals
+            sigma2_nsqrt[famsize] = vectors.dot(vectors.T)
         return sigma2_nsqrt
 
     def predict(self,X):
@@ -326,11 +326,11 @@ class gtarray(object):
         if self.ndim == 2:
             missingness = ma.mean(self.gts.mask,axis=0)
         elif self.ndim == 3:
-            missingness = ma.mean(self.gts.mask[:,0,:])
-        freqs_pass = np.logical_and(self.freqs > min_maf, freqs < (1 - min_maf))
+            missingness = ma.mean(self.gts.mask[:,0,:],axis = 0)
+        freqs_pass = np.logical_and(self.freqs > min_maf, self.freqs < (1 - min_maf))
         print(str(self.freqs.shape[0] - np.sum(freqs_pass)) + ' SNPs with MAF<' + str(min_maf))
         missingness_pass = 100 * missingness < max_missing
-        print(str(freqs.shape[0] - np.sum(missingness_pass)) + ' SNPs with missingness >' + str(max_missing) + '%')
+        print(str(self.freqs.shape[0] - np.sum(missingness_pass)) + ' SNPs with missingness >' + str(max_missing) + '%')
         filter_pass = np.logical_and(freqs_pass, missingness_pass)
         self.freqs = self.freqs[filter_pass]
         if self.ndim == 2:
@@ -413,13 +413,13 @@ class gtarray(object):
         return gtarray(add_gts,ids_out,self.sid,alleles = self.alleles, fams = self.fams[self_index])
 
     def diagonalise(self,inv_root):
-        if fams is None:
+        if self.fams is None:
             raise(ValueError('Family labels needed for diagonalization'))
         if not self.mean_normalised:
             self.mean_normalise()
         if self.has_NAs:
             self.fill_NAs()
-        unique_fams, famsizes = np.unique(self.fams)
+        unique_fams, famsizes = np.unique(self.fams, return_counts = True)
         fam_indices = dict()
         # Transform
         for fam in unique_fams: