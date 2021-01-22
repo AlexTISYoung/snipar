930a6ad322828bad6413cb2591121dd034e321bf
Alexander Young <alextisyoung@gmail.com>
2021-01-22 13:50:49 -0800

    Update conf.py
    update documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
 # -*- coding: utf-8 -*-
 #
 # Configuration file for the Sphinx documentation builder.
@@ -19,8 +22,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = u'sibreg'
-copyright = u'2020, Alexander Young'
+project = u'SNIPar'
+copyright = u'2021, Alexander Young'
 author = u'Alexander Young'
 
 # The short X.Y version
@@ -43,6 +46,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -78,7 +82,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
