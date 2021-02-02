e003a067f332b18242b8ce76803875289335df0f
Hariharan Jayashankar <hariharan.jayashankar@gmail.com>
2021-02-03 00:13:08 +0530

    added more plots

--- ldsc_reg/plotfunctions/multigen_sims_05012020.R
+++ ldsc_reg/plotfunctions/multigen_sims_05012020.R
@@ -4,35 +4,96 @@ library(latex2exp)
 source("ldsc_reg/plotfunctions/helperfuncs.R")
 
 
-log_files <- Sys.glob("ldsc_reg/multigen_sim/05012020/[0-9]*.log")
-simdf <- readlogfiles(log_files)
+# Reading in Data ---------------------------------------------------------
 
-true_sd <- sd(simdf$r)
-true_se <- true_sd/sqrt(nrow(simdf))
 
-simdf %>% 
-    ggplot() +
-    geom_point(aes(traitcode, r_invhse, color = "Inverse Hessian")) +
-    geom_point(aes(traitcode, r_jkse, color = "Block Jack Knife")) +
-    geom_hline(yintercept = true_sd) +
-    geom_text(aes(x = 50, y = true_sd + 0.0006, label = "True SD")) +
-    geom_hline(yintercept = true_se) +
-    geom_text(aes(x = 50, y = true_se + 0.0006, label = "True SE")) +
+log_files_avgparental <- Sys.glob("ldsc_reg/multigen_sim/05012020/*_avgparental.log")
+avg_parental <- readlogfiles(log_files_avgparental)
+
+log_files_pop <- Sys.glob("ldsc_reg/multigen_sim/05012020/*_pop.log")
+pop <- readlogfiles(log_files_pop)
+
+ldsc_estimates <- read_csv("ldsc_reg/multigen_sim/05012020/ldsc_estimates.csv")
+
+true_sd_avgpar_snipar <- sd(avg_parental$r)
+true_sd_pop_snipar <- sd(pop$r)
+
+true_sd_avgpar_ldsc <- ldsc_estimates %>% 
+    filter(effect_estimated == "Average Parental") %>% 
+    pull(estimate) %>% 
+    sd
+true_sd_pop_ldsc <- ldsc_estimates %>% 
+    filter(effect_estimated == "Population") %>% 
+    pull(estimate) %>% 
+    sd
+
+
+# Plotting ----------------------------------------------------------------
+
+# avg parental
+ggplot() +
+    geom_point(data = avg_parental, 
+               aes(traitcode, r_invhse, color = "Inverse Hessian")) +
+    geom_point(data = avg_parental, 
+               aes(traitcode, r_jkse, color = "Block Jack Knife")) +
+    geom_point(data = ldsc_estimates %>% filter(effect_estimated == "Average Parental"), 
+               aes(run, se, color = "LDSC Block Jack Knife")) +
+    geom_hline(yintercept = true_sd_avgpar_snipar) +
+    geom_text(aes(x = 50, y = true_sd_avgpar_snipar - 0.005, label = "True SD - SNIPar")) +
+    geom_hline(yintercept = true_sd_avgpar_ldsc) +
+    geom_text(aes(x = 50, y = true_sd_avgpar_ldsc + 0.005, label = "True SD - LDSC")) +
     scale_color_brewer(palette = "Dark2") +
     theme_minimal() +
     labs(x = "Run Number", y = "Standard Error Estimate",
          color = "")
-ggsave("ldsc_reg/multigen_sim/05012020/comparing_se.png",
+
+ggsave("ldsc_reg/multigen_sim/05012020/se_avgparental.png",
        height = 6, width = 9)   
 
-
-simdf %>% 
-    ggplot() +
-    geom_point(aes(traitcode, r, color = "Estimates")) +
-    guides(color = F) +
-    theme_minimal() +
+ggplot() +
+    geom_point(data = pop, 
+               aes(traitcode, r_invhse, color = "Inverse Hessian")) +
+    geom_point(data = pop, 
+               aes(traitcode, r_jkse, color = "Block Jack Knife")) +
+    geom_point(data = ldsc_estimates %>% filter(effect_estimated == "Population"), 
+               aes(run, se, color = "LDSC Block Jack Knife")) +
+    geom_hline(yintercept = true_sd_pop_snipar) +
+    geom_text(aes(x = 50, y = true_sd_pop_snipar + 0.002, label = "True SD - SNIPar")) +
+    geom_hline(yintercept = true_sd_pop_ldsc) +
+    geom_text(aes(x = 50, y = true_sd_pop_ldsc - 0.002, label = "True SD - LDSC")) +
     scale_color_brewer(palette = "Dark2") +
-    labs(x = "Run", y = TeX("$\\textit{r_g}$ Direct + Population"))
+    theme_minimal() +
+    labs(x = "Run Number", y = "Standard Error Estimate",
+         color = "")
 
-ggsave("ldsc_reg/multigen_sim/05012020/rg_estimate.png",
+ggsave("ldsc_reg/multigen_sim/05012020/se_pop.png",
+       height = 6, width = 9)   
+
+# Comparing estimates
+ggplot() +
+    geom_point(data = pop, 
+               aes(traitcode, r, color = "SNIPar")) +
+    geom_point(data = ldsc_estimates %>% filter(effect_estimated == "Population"), 
+               aes(run, estimate, color = "LDSC")) +
+    geom_hline(yintercept = 1.0) +
+    scale_color_brewer(palette = "Dark2") +
+    theme_minimal() +
+    labs(x = "Run Number", y = "Estimate",
+         color = "")
+
+ggsave("ldsc_reg/multigen_sim/05012020/estimates_pop.png",
+       height = 6, width = 9)  
+
+ggplot() +
+    geom_point(data = avg_parental, 
+               aes(traitcode, r, color = "SNIPar")) +
+    geom_point(data = ldsc_estimates %>% filter(effect_estimated == "Average Parental"), 
+               aes(run, estimate, color = "LDSC")) +
+    geom_hline(yintercept = 0.5) +
+    scale_color_brewer(palette = "Dark2") +
+    theme_minimal() +
+    labs(x = "Run Number", y = "Estimate",
+         color = "")
+
+ggsave("ldsc_reg/multigen_sim/05012020/estimates_avgparental.png",
        height = 6, width = 9)   
