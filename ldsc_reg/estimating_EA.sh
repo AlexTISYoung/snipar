ff6fc6eefc8ddbf98111230ff2d15b22ce7681dc
Hariharan Jayashankar <harij@g03.nber.org>
2021-02-24 04:18:15 -0500

    cleaned up a lot of unecessary files

--- ldsc_reg/estimating_EA.sh
+++
@@ -1,6 +0,0 @@
-sniparpath="/homes/nber/harij/gitrepos/SNIPar"
-
-python "${sniparpath}/ldsc_reg/run_estimates.py" \
-        "/disk/genetics/ukb/alextisyoung/haplotypes/relatives/EA/chr_*.hdf5" \
-        -ldsc "/disk/genetics/ukb/alextisyoung/haplotypes/relatives/bedfiles/ldscores/*[0-9].l2.ldscore.gz" \
-        -l "${sniparpath}/ldsc_reg/estimating_EA.log" 
