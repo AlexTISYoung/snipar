f57c767464fba39bc505cb349c9d645c5efba3af
ay <alextisyoung@gmail.com>
2022-01-19 19:07:38 +0000

    a

--- snipar/__init__.py
+++
@@ -1,6 +0,0 @@
-import snipar.gtarray as gtarray
-import snipar.ibd as ibd
-import snipar.lmm as lmm
-import snipar.pgs as pgs
-import snipar.preprocess as preprocess
-import snipar.utilities as utilities
--- snipar/preprocess.py
+++ snipar/preprocess.py
@@ -7,6 +7,7 @@ import numpy as np
 from snipar.utilities import make_id_dict
 import pandas as pd
 import logging
+import snipar
 
 def parse_obsfiles(obsfiles, obsformat='bed'):
     obs_files = []
