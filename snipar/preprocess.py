fix issue with zero variance genotyping error estimates, and update IBD part of tutorial

snipar/preprocess.py
@@ -302,7 +302,7 @@ def estimate_genotyping_error_rate(bedfiles, ped, min_maf):
     # Estimate mean and variance of MLE error estimates
     mean_error = np.mean(genome_error_rates)
     var_error = np.var(genome_error_rates)
-    if var_error>0:
+    if var_error > 0:
         mean_inv_het = np.mean(1/sum_het)
         beta = 1/(var_error/mean_error-mean_inv_het)
         if beta > 0: