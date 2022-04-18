df630d44506aa84d08496a6f1f361db6abe8db7b
moeen-n <nehzati@usc.edu>
2022-04-18 14:32:40 -0400

    fix a bug in tests.__init__.py

--- snipar/tests/__init__.py
+++ snipar/tests/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from snipar.tests.test_sibreg import *
 from snipar.tests.test_impute_from_sibs import *
-from snipar.tests.test_commandline import *
+from snipar.tests.test_impute import *
 from snipar.tests.test_pedigree_creation import *
 from snipar.tests.test_example import *
 
