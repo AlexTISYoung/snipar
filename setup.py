36b8eb8d6a079324d54f6c9fef95708db75df6c3
moeen-n <nehzati@usc.edu>
2022-04-18 15:11:52 -0400

    use python_requires in setup.py

--- setup.py
+++ setup.py
@@ -45,10 +45,8 @@ setup(name='snipar',
             # Pick your license as you wish (should match "license" above)
             'License :: OSI Approved :: MIT License',
 
-            # Specify the Python versions you support here. In particular, ensure
-            # that you indicate whether you support Python 2, Python 3 or both.
-            'Programming Language :: Python :: 3.7+',
       ],
+      python_requires='>=3.7',
       keywords='statistics genetics',
       packages=['snipar', 'snipar.imputation', 'snipar.read', 'snipar.tests', 'snipar.example', 'snipar.scripts'],
       install_requires=[
