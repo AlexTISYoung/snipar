947026e56a03648e524bc4b63a23ba1880036faf
ay <alextisyoung@gmail.com>
2022-04-18 12:04:55 -0700

    change simulate_pop_simple and generate_tutorial

--- snipar/tests/generate_tutorial.sh
+++ snipar/tests/generate_tutorial.sh
@@ -1,22 +1,41 @@
 # Simulate population genotypes
-python example/simulate_pop_simple.py 1000 0.5 3000 1000 1000 0.5 example/sample --chrom 1
+python ../snipar/simulate/simulate_pop_simple.py 1000 0.5 10000 0 10000 0 chr_1 --chrom 1 --blocksize 1
 # Convert to bgen
-/disk/genetics/ukb/alextisyoung/qctool/build/release/qctool_v2.0.7 -g example/sample.haps -s example/sample.sample -og example/sample.bgen -os example/sample.sample -filetype shapeit_haplotypes -ofiletype bgen
+qctool_v2.2.0-osx/qctool -g chr_1.haps -s chr_1.sample -og chr_1.bgen -os chr_1.sample -filetype shapeit_haplotypes -ofiletype bgen
 # Convert to bed
-plink/plink2 --bgen example/sample.bgen ref-last --sample example/sample.sample --make-bed --out example/sample --oxford-single-chr 1
+./plink2 --bgen chr_1.bgen ref-last --sample chr_1.sample --make-bed --out chr_1 --oxford-single-chr 1
 # Simulate phenotype
-python example/simulate_trait_quad.py example/sample.bed example/sample.ped 0.8 example/h2_quad_0.8 --no_sib --dncor 0.5
+python ../snipar/simulate/simulate_trait_quad.py chr_1.bed chr_1.ped 0.8 phenotype --no_sib --dncor 0.5
 # Remove some parents
-/disk/genetics/ukb/alextisyoung/qctool/build/release/qctool_v2.0.7 -g example/sample.bgen -s example/sample.sample -og example/sample_reduced.bgen -os example/sample_reduced.sample -filetype bgen -ofiletype bgen -excl-samples example/sample_remove.txt
+qctool_v2.2.0-osx/qctool -g chr_1.bgen -s chr_1.sample -og chr_1_reduced.bgen -os chr_1_reduced.sample -filetype bgen -ofiletype bgen -excl-samples chr_1_remove.txt
 # Convert reduced to bed
-plink/plink2 --bgen example/sample_reduced.bgen ref-last --sample example/sample_reduced.sample --make-bed --out example/sample_reduced --oxford-single-chr 1
+./plink2 --bgen chr_1_reduced.bgen ref-last --sample chr_1_reduced.sample --make-bed --out chr_1_reduced --oxford-single-chr 1
 # Clean up
-rm example/sample.haps
-mv example/sample_reduced.bgen example/sample.bgen
-mv example/sample_reduced.sample example/sample.sample
-mv example/sample_reduced.bed example/sample.bed
-mv example/sample_reduced.fam example/sample.fam
-mv example/sample_reduced.bim example/sample.bim
-rm example/sample_remove.txt
-rm example/sample.log
-rm example/sample_reduced.log
+rm chr_1.haps
+mv chr_1_reduced.bgen chr_1.bgen
+mv chr_1_reduced.sample chr_1.sample
+mv chr_1_reduced.bed chr_1.bed
+mv chr_1_reduced.fam chr_1.fam
+mv chr_1_reduced.bim chr_1.bim
+rm chr_1_remove.txt
+rm chr_1.log
+rm chr_1_reduced.log
+mv phenotype.direct_weights.txt direct_weights.txt
+mv chr_1.genetic_map.txt genetic_map.txt
+mv chr_1.king.kin0 king.kin0
+mv chr_1.king.segments.gz king.segments.gz
+mv chr_1.chr_1.ibd.segments.gz chr_1.ibd.segments.gz
+mv chr_1.kingallsegs.txt kingallsegs.txt
+mv chr_1.ped pedigree.txt
+mv chr_1.agesex agesex.txt
+
+## Analyse
+impute.py --ibd chr_1.ibd --bed chr_@ --king king.kin0 --agesex agesex.txt --out chr_@ --threads 4 
+#impute.py --ibd king --bed chr_@ --king king.kin0 --agesex agesex.txt --out chr_@ --threads 4 --ibd_is_king
+gwas.py phenotype.txt --bed chr_@ --imp chr_@ --threads 4
+python ../snipar/example/example_data/estimate_sim_effects.py chr_1.sumstats.hdf5 phenotype.effects.txt
+pgs.py direct --bed chr_@ --imp chr_@ --weights direct_weights.txt
+pgs.py direct --pgs direct.pgs.txt --phenofile phenotype.txt
+pgs.py direct_sib --bed chr_1 --imp chr_1 --weights direct_weights.txt --phenofile phenotype.txt --fit_sib
+## Clean
+#rm *txt king* chr_1* 
