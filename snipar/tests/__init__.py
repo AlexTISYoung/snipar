64d2735f0a1fdff1e4d74fda792e310a06aabff1
moeen-n <nehzati@usc.edu>
2022-04-18 14:20:51 -0400

    Add test_example

--- snipar/tests/__init__.py
+++ snipar/tests/__init__.py
@@ -4,6 +4,7 @@ from snipar.tests.test_sibreg import *
 from snipar.tests.test_impute_from_sibs import *
 from snipar.tests.test_commandline import *
 from snipar.tests.test_pedigree_creation import *
+from snipar.tests.test_example import *
 
 if __name__ == '__main__':
     unittest.main()
