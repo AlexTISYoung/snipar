236fb461b1a2f1ee72975a464f6c9f0d0fd679ff
ay <alextisyoung@gmail.com>
2022-08-17 16:38:49 -0700

    remove botnia gs scripts

--- Botnia/sbayesr_to_snipar.R
+++
@@ -1,9 +0,0 @@
-### Get SNP IDs from SBayesR to snipar ###
-setwd('/ludc/Active_Projects/BOTNIA_AYoung_analysis/Private/pgs/')
-d=read.table('EA4_2.8m.snpRes',header=T)
-bim=read.table('../haplotypes/bedfiles/autosome.bim',header=F)
-bim=cbind(bim,paste(bim[,1],':',bim[,4],sep=''))
-d$chrpos = paste(d$Chrom,d$Position,sep=':')
-d_out = data.frame(SNP=bim[match(d$chrpos,bim[,7]),2],
-                    A1=d$A1,A2=d$A2,beta=d$A1Effect)
-write.table(d_out,'EA4_2.8m.txt',quote=F,row.names=F)
--- GS/PCA/snipar_analysis.sh
+++
@@ -1,9 +0,0 @@
-# /disk/genetics/sibling_consortium/GS20k/alextisyoung/HM3/tidy
-source env/bin/activate
-ibd.py --bgen ../genotypes/haplotypes/chr_@_haps --pedigree ../../pedigree.txt --out ibd/chr_@ --ld_out --threads 20
-impute.py --ibd ibd/chr_@.ibd --bgen ../genotypes/haplotypes/chr_@_haps --pedigree ../../pedigree.txt --threads 20 --out imputed/chr_@
-for i in {14..16}
-do
-mkdir traits/$i
-gwas.py ../../processed_traits.fam --out traits/$i/chr_@ --bgen ../genotypes/haplotypes/chr_@_haps --imp imputed/chr_@ --covar ../../covariates.fam --phen_index $i --threads 20
-done
