3c465de1d445387d022d84c8b946b3fd6a5e9fb8
Alexander Young <alextisyoung@gmail.com>
2023-11-23 17:31:19 -0500

    fix output for 3 generation pgs analysis

--- snipar/pgs.py
+++ snipar/pgs.py
@@ -683,7 +683,6 @@ def fit_pgs_model(y, pg, ngen, ibdrel_path=None, covariates=None, fit_sib=False,
             alpha, alpha_cols = make_and_fit_model(y, pg, pg_cols_m, ibdrel_path=ibdrel_path, covariates=covariates, sparse_thresh=sparse_thresh)
             write_estimates(outprefix+'.'+str(ngen)+'.maternal', alpha, alpha_cols)
             alpha, alpha_cols = make_and_fit_model(y, pg, pg_cols_b, ibdrel_path=ibdrel_path, covariates=covariates, sparse_thresh=sparse_thresh)
-            write_estimates(outprefix+'.'+str(ngen)+'.both', alpha, alpha_cols)
     # Save to file
     if outprefix is not None:
         write_estimates(outprefix+'.'+str(ngen), alpha, alpha_cols)
